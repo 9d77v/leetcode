ce52ffd5b3e878efc8932577b957cd50785857ff
9d77v <9d77v@9d77v.me>
2020-12-29 22:27:00 +0800

    feat: add 剑指Offer 12

---
+++ 剑指Offer/12.矩阵中的路径/main_test.go
@@ -0,0 +1,36 @@
+package main
+
+import "testing"
+
+func Test_exist(t *testing.T) {
+	type args struct {
+		board [][]byte
+		word  string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{"should be true", args{
+			[][]byte{
+				{'A', 'B', 'C', 'E'},
+				{'S', 'F', 'C', 'S'},
+				{'A', 'D', 'E', 'E'},
+			}, "ABCCED",
+		}, true},
+		{"should be false", args{
+			[][]byte{
+				{'a', 'b'},
+				{'c', 'd'},
+			}, "abcd",
+		}, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := exist(tt.args.board, tt.args.word); got != tt.want {
+				t.Errorf("exist() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
