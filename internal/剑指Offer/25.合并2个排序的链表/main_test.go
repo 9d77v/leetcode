2dbfe07741ece8fce6bcaf997bdcea203a19fcec
9d77v <9d77v@9d77v.me>
2021-01-10 22:17:51 +0800

    feat: add 剑指Offer 25

---
+++ internal/剑指Offer/25.合并2个排序的链表/main_test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/9d77v/leetcode/pkg/algorithm/singlylinkedlist"
+)
+
+type args struct {
+	l1 *ListNode
+	l2 *ListNode
+}
+
+var tests = []struct {
+	name string
+	args args
+	want []int
+}{
+	{"should be equal", args{NewList([]int{1, 2, 3}), NewList([]int{1, 3, 4})}, []int{1, 1, 2, 3, 3, 4}},
+}
+
+func Test_mergeTwoListsFunc1(t *testing.T) {
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := mergeTwoListsFunc1(tt.args.l1, tt.args.l2); !reflect.DeepEqual(got.ToArray(), tt.want) {
+				t.Errorf("mergeTwoListsFunc1() = %v, want %v", got.ToArray(), tt.want)
+			}
+		})
+	}
+}
+
+func Test_mergeTwoListsFunc2(t *testing.T) {
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := mergeTwoListsFunc2(tt.args.l1, tt.args.l2); !reflect.DeepEqual(got.ToArray(), tt.want) {
+				t.Errorf("mergeTwoListsFunc2() = %v, want %v", got.ToArray(), tt.want)
+			}
+		})
+	}
+}
