796cfe0984647c753cd362b50bce8b9bebbcdb1d
9d77v <9d77v@9d77v.me>
2021-03-01 07:51:34 +0800

    feat: add leetcode 303

---
+++ internal/leetcode/303.range-sum-query-immutable/main.go
@@ -0,0 +1,51 @@
+package main
+
+/*
+题目：
+给定一个整数数组  nums，求出数组从索引 i 到 j（i ≤ j）范围内元素的总和，包含 i、j 两点。
+
+实现 NumArray 类：
+
+NumArray(int[] nums) 使用数组 nums 初始化对象
+int sumRange(int i, int j) 返回数组 nums 从索引 i 到 j（i ≤ j）范围内元素的总和，包含 i、j 两点（也就是 sum(nums[i], nums[i + 1], ... , nums[j])）
+
+提示：
+
+0 <= nums.length <= 104
+-105 <= nums[i] <= 105
+0 <= i <= j < nums.length
+最多调用 104 次 sumRange 方法
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/range-sum-query-immutable
+*/
+type NumArray struct {
+	preSum []int
+}
+
+func Constructor(nums []int) NumArray {
+	arr := NumArray{
+		preSum: make([]int, len(nums)),
+	}
+	for i, num := range nums {
+		if i > 0 {
+			arr.preSum[i] = arr.preSum[i-1] + num
+		} else {
+			arr.preSum[i] = num
+		}
+	}
+	return arr
+}
+
+func (this *NumArray) SumRange(i int, j int) int {
+	if i > 0 {
+		return this.preSum[j] - this.preSum[i-1]
+	}
+	return this.preSum[j]
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * obj := Constructor(nums);
+ * param_1 := obj.SumRange(i,j);
+ */
